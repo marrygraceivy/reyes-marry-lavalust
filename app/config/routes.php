be1a50f623fddcf84bcdec35b9d2434f7381ebfd
Marry Grace Ivy Reyes <marrygraceivyr@gmail.com>
2026-09-09 22:08:04 +0800

    Laboratory Exercise 5

--- app/config/routes.php
+++ app/config/routes.php
@@ -43,7 +43,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |
 */
 /** @var object $router **/
-
+$router->get('/', 'Welcome::index');
 $router->get('/login', 'AuthController::login');
 $router->post('/login', 'AuthController::login');
 $router->get('/logout', 'AuthController::logout');
