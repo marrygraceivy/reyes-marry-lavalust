84ad823d6e187646f8f069d4ab85175232d9f088
Marry Grace Ivy Reyes <marrygraceivyr@gmail.com>
2026-09-10 06:48:15 +0800

    Lab Exercise 5

--- app/config/config.php
+++ app/config/config.php
@@ -26,8 +26,11 @@ $config['environment'] = getenv('APP_ENV') ?: 'development';
 | | Base Site URL
 | |--------------------------------------------------------------------------
 */
+$protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? "https://" : "http://";
+$root = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
+$path = str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 
-$config['base_url'] = 'http://localhost/lavalust/LavaLust/';
+$config['base_url'] = $protocol . $root . $path;
 
 /*
 | |--------------------------------------------------------------------------
